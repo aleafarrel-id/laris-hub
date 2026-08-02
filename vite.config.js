e730a5352174b7b6e7cee6be0d371b4e1e022e2b
Alea Farrel <aleafarrel4@gmail.com>
2026-08-02 21:17:29 +0700

    change icon siz

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
                 enabled: true,
                 type: 'module',
             },
-            includeAssets: ['favicon.ico', 'favicon.svg', 'logo-192.png', 'logo-512.png', 'robots.txt'],
+            includeAssets: ['favicon.ico', 'favicon.svg', 'logo-192.png', 'robots.txt'],
             manifest: {
                 name: 'Laris Hub',
                 short_name: 'Laris Hub',
@@ -37,18 +37,6 @@ export default defineConfig({
                         src: 'logo-192.png',
                         sizes: '192x192',
                         type: 'image/png',
-                    },
-                    {
-                        src: 'logo-512.png',
-                        sizes: '512x512',
-                        type: 'image/png',
-                        purpose: 'any',
-                    },
-                    {
-                        src: 'logo-512.png',
-                        sizes: '512x512',
-                        type: 'image/png',
-                        purpose: 'maskable',
                     },
                 ],
                 shortcuts: [
--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
         enabled: true,
         type: 'module',
       },
-      includeAssets: ['favicon.ico', 'favicon.svg', 'logo-192.png', 'logo-512.png', 'robots.txt'],
+      includeAssets: ['favicon.ico', 'favicon.svg', 'logo-192.png', 'robots.txt'],
       manifest: {
         name: 'Laris Hub',
         short_name: 'Laris Hub',
@@ -38,18 +38,6 @@ export default defineConfig({
             src: 'logo-192.png',
             sizes: '192x192',
             type: 'image/png',
-          },
-          {
-            src: 'logo-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any',
-          },
-          {
-            src: 'logo-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
           },
         ],
         shortcuts: [
